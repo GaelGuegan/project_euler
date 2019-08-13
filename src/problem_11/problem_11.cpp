1b7a27b8c6dcc421fddb4a7ff3a1e1984456b7fd
GaelGuegan <gaelguegan1995@gmail.com>
2019-08-13 22:12:57 +0200

    Add problem_11 solving
    Signed-off-by: GaelGuegan <gaelguegan1995@gmail.com>

---
+++ src/problem_11/problem_11.cpp
@@ -0,0 +1,91 @@
+#include "problem_11.h"
+#include "utils.h"
+#include <sstream>
+
+void get_2d_vector_from_file(const char* filename, vector<vector<int>> &input)
+{
+	ifstream file(filename);
+    string line;
+
+    while(getline(file, line, '\n')) {
+
+    	vector<int> v;
+    	stringstream line_stream;
+    	line_stream.str(line);
+    	string num;
+
+	    while (getline(line_stream, num, ' ')) {
+	    	v.push_back(stoi(num));
+	    }
+	    input.push_back(v);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+    if (parse_opts(argc, argv) != 0)
+        exit(1);
+
+    vector<vector<int>> input;
+    double mult = 1;
+
+    get_2d_vector_from_file(opt.file, input);
+
+    // HORIZONTAL
+    mult = 1;
+    for (int i = 0; i < input.size(); i++) {
+    	for (int j = 0 ; j < input[i].size() - opt.num; j++) {
+    		double tmp = 1;
+    		for (int k = 0; k < opt.num; k++) {
+    			tmp *= input[i][j + k];
+    		}
+    		if (tmp > mult)
+    			mult = tmp;
+    	}
+    }
+    cout << "horizontal: " << fixed << mult << endl;
+
+    // VERTICAL
+    mult = 1;
+    for (int i = 0; i < input.size() - opt.num; i++) {
+    	for (int j = 0 ; j < input[i].size(); j++) {
+    		double tmp = 1;
+    		for (int k = 0; k < opt.num; k++) {
+    			tmp *= input[i + k][j];
+    		}
+    		if (tmp > mult)
+    			mult = tmp;
+    	}
+    }
+    cout << "vertical  : " << fixed << mult << endl;
+
+    // DIAGONAL 1
+    mult = 1;
+    for (int i = 0; i < input.size() - opt.num - 1; i++) {
+    	for (int j = 0 ; j < input[i].size(); j++) {
+    		double tmp = 1;
+    		for (int k = 0; k < opt.num; k++) {
+    			tmp *= input[i + k][j + k];
+    		}
+    		if (tmp > mult)
+    			mult = tmp;
+    	}
+    }
+    cout << "diago 1   : "<< fixed  << mult << endl;
+
+    // DIAGONAL 2
+    mult = 1;
+    for (int i = 0; i < input.size() - opt.num; i++) {
+    	for (int j = input.size() - 1; j >= opt.num - 1; j--) {
+    		double tmp = 1;
+    		for (int k = 0; k < opt.num; k++) {
+    			tmp *= input[i + k][j - k];
+    		}
+    		if (tmp > mult)
+    			mult = tmp;
+    	}
+    }
+    cout << "diago 2   : " << fixed << mult << endl;
+
+    return 0;
+}
---
+++ src/problem_11/problem_11.h
@@ -0,0 +1,72 @@
+#ifndef PROBLEM_11
+#define PROBLEM_11
+
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <getopt.h>
+#include <vector>
+
+using namespace std;
+
+const char *help =
+    "Usage: [options]\n"
+    "Options:\n"
+    "    -f, --file    Input file containing large number (default: input.txt)\n"
+    "    -n, --num     How many numbers to sum            (default: 4)\n"
+    "    -h, --help    print help\n"
+    "\n";
+
+static struct option long_options[] =
+{
+    {"file",  required_argument,  0, 'f'},
+    {"help",  no_argument,        0, 'h'},
+    {0, 0, 0, 0}
+};
+
+static struct opt {
+    const char *file;
+    int num;
+} opt;
+
+int parse_opts(int argc, char **argv)
+{
+    /* set the default values */
+    opt.file = "/home/gaelguegan/Desktop/project_euler/src/problem_11/input.txt";
+    opt.num = 4;
+
+    /* parse the options */
+    int c;
+    int opt_idx = 0;
+    while (-1 != (c = getopt_long(argc, argv, "f:h",
+                                  long_options, &opt_idx))) {
+        switch(c) {
+        case 'h':
+            printf("%s", help);
+            exit(0);
+        case 'f':
+            opt.file = optarg;
+            break;
+        case 's':
+            sscanf(optarg, "%i", &opt.num);
+            break;
+        default:
+            cerr << "Unknow option." << endl;
+            exit(1);
+        }
+     }
+     
+    if (optind < argc)
+    {
+        cout << "non-option ARGV-elements: ";
+        while (optind < argc)
+            cout << argv[optind++];
+        cout << endl;
+    }
+    return 0;
+}
+
+#endif
