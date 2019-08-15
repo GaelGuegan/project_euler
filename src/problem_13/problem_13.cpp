dd949fcebdd0314dd2677ee4d142b7326f2d166d
GaelGuegan <gaelguegan1995@gmail.com>
2019-08-15 13:05:13 +0200

    Add problem_13 solving
    Signed-off-by: GaelGuegan <gaelguegan1995@gmail.com>

---
+++ src/problem_13/problem_13.cpp
@@ -0,0 +1,39 @@
+#include "problem_13.h"
+#include "utils.h"
+
+int main(int argc, char* argv[])
+{
+    if (parse_opts(argc, argv) != 0)
+        exit(1);
+
+    ifstream file(opt.file);
+    string line;
+    vector<string> v;
+    vector<int> w;
+    int unit = 0;
+    int carry = 0;
+    int sum_tmp = 0;
+    int val = 0;
+
+    while(getline(file, line)) {
+        v.push_back(line);
+    }
+
+    for (int i = v[0].size() - 1; i >= 0; i--) {
+        unit = 0;
+        sum_tmp = 0;
+        for (int j = 0; j < v.size(); j++) {
+            sum_tmp += v[j][i] - '0';
+        }
+        unit = sum_tmp % 10;
+        w.push_back((unit + carry) % 10);
+        carry = (sum_tmp + carry) / 10;
+    }
+    w.push_back(carry);
+
+    for (int i = w.size() - 1; i >= 0; i--)
+        cout << w[i]  << flush;
+    cout << endl;
+
+    return 0;
+}
---
+++ src/problem_13/problem_13.h
@@ -0,0 +1,66 @@
+#ifndef PROBLEM_13
+#define PROBLEM_13
+
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <getopt.h>
+#include <vector>
+
+using namespace std;
+
+const char *help =
+    "Usage: [options]\n"
+    "Options:\n"
+    "    -f, --file    input file (default: input.txt)\n"
+    "    -h, --help    print help\n"
+    "\n";
+
+static struct option long_options[] =
+{
+    {"file",  required_argument,  0, 'f'},
+    {"help",   no_argument,       0, 'h'},
+    {0, 0, 0, 0}
+};
+
+static struct opt {
+    const char* file;
+} opt;
+
+int parse_opts(int argc, char **argv)
+{
+    /* set the default values */
+    opt.file = "/home/gaelguegan/Desktop/project_euler/src/problem_13/input.txt";
+
+    /* parse the options */
+    int c;
+    int opt_idx = 0;
+    while (-1 != (c = getopt_long(argc, argv, "f:h",
+                                  long_options, &opt_idx))) {
+        switch(c) {
+        case 'h':
+            printf("%s", help);
+            exit(0);
+        case 'f':
+            opt.file = optarg;
+            break;
+        default:
+            cerr << "Unknow option." << endl;
+            exit(1);
+        }
+     }
+     
+    if (optind < argc)
+    {
+        cout << "non-option ARGV-elements: ";
+        while (optind < argc)
+            cout << argv[optind++];
+        cout << endl;
+    }
+    return 0;
+}
+
+#endif
