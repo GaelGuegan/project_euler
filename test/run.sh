e2e79b2c62a75e9b699fd493dc2f5cebb9218633
GaelGuegan <gael.guegan-ext@sagemcom.com>
2021-01-27 15:20:07 +0100

    Add test colors
    Signed-off-by: GaelGuegan <gael.guegan-ext@sagemcom.com>

--- test/run.sh
+++ test/run.sh
@@ -4,9 +4,9 @@ BIN_DIR="$PWD"/build/src
 
 for f in "$BIN_DIR"/problem_*/problem_*
 do
-    echo "================"
+    echo -e "\e[32m================"
     basename "$f"
-    echo "================"
+    echo -e "================\e[39m"
     "$f"
     echo ""
 done
