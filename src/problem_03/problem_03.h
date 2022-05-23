f20abf0a8f2c327fbc598c6d187696c68b1b5dff
Gael GUEGAN <gael.guegan-ext@sagemcom.com>
2022-05-23 16:18:36 +0200

    Add copyright

--- src/problem_03/problem_03.h
+++ src/problem_03/problem_03.h
@@ -1,3 +1,12 @@
+/*************************************************************************
+ * Copyright (c) 2022 Gael GUEGAN. All rights reserved.
+ *
+ * The computer program contained herein contains proprietary information
+ * which is the property of Me.
+ * The program may be used and/or copied only with the written permission
+ * of Me or in accordance with the terms and conditions stipulated
+ * in the agreement/contract under which the programs have been supplied.
+ *************************************************************************/
 #ifndef PROBLEM_3
 #define PROBLEM_3
 
